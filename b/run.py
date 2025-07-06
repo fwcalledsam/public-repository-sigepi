5bca56583a22c97fea187e0edbec5cad88e147c4
/Bimori <106877704+Bimori404@users.noreply.github.com>
2025-07-05 22:08:05 -0600

    feat: Añadir configuración para despliegue en Vercel y crear manejador de solicitudes

--- b/run.py
+++ b/run.py
@@ -1,10 +1,7 @@
 from flask import Flask
 import os
 
-# --------------------
 from config import Config
-
-# --------------------
 from models import db
 from routes import api
 
@@ -12,8 +9,12 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 
+# Configuración de la carpeta de uploads
 app.config["UPLOAD_FOLDER"] = os.path.join(os.getcwd(), "uploads")
+os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
+
 app.register_blueprint(api, url_prefix="/")
 
+# Para ejecución local
 if __name__ == "__main__":
     app.run(debug=True)
