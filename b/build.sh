254099e96175f99f8571bc7b617e3d211a4849cd
/Bimori <106877704+Bimori404@users.noreply.github.com>
2025-07-06 08:30:59 -0600

    test

--- b/build.sh
+++ b/build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# Instala gunicorn primero
+pip install gunicorn==20.1.0  # Puedes especificar una versión o dejarlo sin versión
+# Luego instala los demás requirements
 pip install -r requirements.txt
