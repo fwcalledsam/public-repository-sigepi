5ea5cb8ecc3b6838992c0fdbbb1ea66de97589b5
/Bimori <106877704+Bimori404@users.noreply.github.com>
2025-07-06 09:06:46 -0600

    test

--- b/build.sh
+++ b/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-# Instalar dependencias del sistema
 apt-get update && apt-get install -y libpq-dev gcc python3-dev
-
-# Instalar dependencias de Python incluyendo gunicorn
 python -m pip install --upgrade pip
 python -m pip install --no-cache-dir -r requirements.txt
-python -m pip install --user gunicorn==20.1.0
+python -m pip install gunicorn==20.1.0
